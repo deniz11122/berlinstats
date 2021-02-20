8711a2c4e65c027bbc1c14e0904de12fd448c744
deniz11122 <76531667+deniz11122@users.noreply.github.com>
2021-02-21 01:12:38 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,8 @@
+echo off
+title Alf Stat
+color a
+cls
+:a
+node bot.js
+goto a
+pause
---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '798539927482531840', // sunucu idsi
+  ownerRole:"798539927653449757", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[798539927520280624], // kayıt olmayanlara verieln rol
+  jailRolu:"797416522560700444", // cezalılara verilen rol
+  enAltYetkiliRolu:[798539927608229897], // ilk yetkili permi
+  activity:"ᛉ Alfheim ❤️ Berlin", // botun durumu
+  status:"idle",
+  invitelink:"discord.gg/alfheim", //davet linki
+  symbol:"ᛉ", // tag
+  voicechannel:"798539929813647393" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
